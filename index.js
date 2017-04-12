4a8a758baa159a5f5c19b5791af86099274a73ad
survivingknowledge <survivingknowledge@gmail.com>
2017-04-12 18:19:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function getTotalAmountForProducts(products, callback, initialValue){
+  let totalPrice = initialValue;
+
+  products.forEach(product => {
+    totalPrice = callback(totalPrice, product);
+  });
+
+  return totalPrice;
+}
+
+function callback(totalPrice, product){
+  if(product.discount >= .5){
+    return totalPrice + product.price;
+  }
+  return totalPrice;
+}
+
+function reduce(collection, callback, initialValue){
+  let result = initialValue;
+
+  collection.forEach(product => {
+    result = callback(result, product);
+  });
+  return result;
+}
+
+
+function reduce(collection, callback, initialValue){
+  let result = initialValue;
+
+  collection.forEach((product, index) => {
+    result = callback(result, product, index, collection);
+  });
+  return result;
+}
